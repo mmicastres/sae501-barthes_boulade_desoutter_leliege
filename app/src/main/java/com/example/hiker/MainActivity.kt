b799a7f51703d21ba51f2eb86d7d11ca9dce87a4
Lyrastico <louisbarthes1@gmail.com>
2023-11-24 10:53:53 +0100

    louis carrie encore comme un goat

--- app/src/main/java/com/example/hiker/MainActivity.kt
+++ app/src/main/java/com/example/hiker/MainActivity.kt
@@ -6,12 +6,23 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+
+private var lon by mutableStateOf<Double?>(null)
+private var lat by mutableStateOf<Double?>(null)
 
 class MainActivity : ComponentActivity() {
 
@@ -21,27 +32,64 @@ class MainActivity : ComponentActivity() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Button(onClick = { fetchLocation() }) {
-                    Text(text = "location")
+                    Text(text = "Location")
                 }
+                DisplayLocation(lon, lat)
             }
         }
+    }
+
+    @Composable
+    fun DisplayLocation(lon : Double?, lat : Double?) {
+        Text(text = "Longitude: ${lon ?: "N/A"}, Latitude: ${lat ?: "N/A"}")
+    }
 
     private fun fetchLocation() {
-        val task = fusedLocationProviderClient.lastLocation
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
 
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
             != PackageManager.PERMISSION_GRANTED && ActivityCompat
-                .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ){
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+                .checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                101
+            )
             return
         }
-        task.addOnSuccessListener {
-            if ( it != null) {
-                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
-                Log.i("LATITUDE", it.latitude.toString())
-            }
-        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : com.google.android.gms.location.LocationCallback() {
+                override fun onLocationResult(locationResult: com.google.android.gms.location.LocationResult) {
+                    val lastLocation = locationResult.lastLocation
+                    lon = lastLocation?.longitude
+                    lat = lastLocation?.latitude
+                    Toast.makeText(
+                        applicationContext,
+                        "${lastLocation?.latitude} ${lastLocation?.longitude}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.i("LATITUDE", lastLocation?.latitude.toString())
+                }
+            },
+            null
+        )
     }
 }
