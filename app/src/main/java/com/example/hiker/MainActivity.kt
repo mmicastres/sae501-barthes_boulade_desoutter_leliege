2c186e8e201ab4f6cff778bc3dfbc857b85697c6
Lyrastico <louisbarthes1@gmail.com>
2023-11-27 17:15:34 +0100

    optimisation de la récupération de la coordonnée actuelle pour la géolocalisation en temps réel

--- app/src/main/java/com/example/hiker/MainActivity.kt
+++ app/src/main/java/com/example/hiker/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.hiker
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,24 +17,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
-import com.example.hiker.model.Location
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.Dispatchers
+import com.google.android.gms.location.*
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.delay
-
-private var lon by mutableStateOf<Double?>(null)
-private var lat by mutableStateOf<Double?>(null)
-
 
 class MainActivity : ComponentActivity() {
 
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private var currentLocations by mutableStateOf(listOf<Location>())
-    private var lastLocations by mutableStateOf(listOf<Location>())
-    private var isLocationUpdatesEnabled by mutableStateOf(false)
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var lon by mutableStateOf<Double?>(null)
+    private var lat by mutableStateOf<Double?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -49,96 +39,88 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Button(onClick = { toggleLocationUpdates() }) {
-                    Text(text = if (isLocationUpdatesEnabled) "Stop Location Updates" else "Start Location Updates")
+                    Text(text = if (lon != null && lat != null) "Stop Location Updates" else "Start Location Updates")
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-                DisplayLocation("Current Location", currentLocations)
-                Spacer(modifier = Modifier.height(16.dp))
-                DisplayLocation("Last Location", lastLocations)
+                if (lon != null && lat != null) {
+                    DisplayLocation("Current Location")
+                }
             }
         }
     }
 
     @Composable
-    fun DisplayLocation(title: String, locations: List<Location>) {
+    fun DisplayLocation(title: String) {
         Column {
             Text(text = "$title:")
-            locations.forEachIndexed { index, location ->
-                Text(text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}")
-            }
+            Text(text = "Latitude: ${lat ?: 0.0}, Longitude: ${lon ?: 0.0}")
         }
     }
 
     private fun toggleLocationUpdates() {
         lifecycleScope.launch {
-            if (isLocationUpdatesEnabled) {
+            if (lon != null && lat != null) {
                 stopLocationUpdates()
             } else {
-                startLocationUpdates()
+                // Vérifiez d'abord l'autorisation
+                if (checkLocationPermission()) {
+                    startLocationUpdates()
+                } else {
+                    // Si l'autorisation n'est pas accordée, demandez-la
+                    requestLocationPermission()
+                }
             }
-            isLocationUpdatesEnabled = !isLocationUpdatesEnabled
         }
     }
 
     private fun startLocationUpdates() {
         lifecycleScope.launch {
-            while (isLocationUpdatesEnabled) {
-                fetchLocation()
-                delay(5000) // Attendez 5 secondes avant de rafraîchir la position
+            val locationRequest = LocationRequest.create().apply {
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                interval = 1000
+            }
+
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    val lastLocation = locationResult.lastLocation
+                    lon = lastLocation?.longitude
+                    lat = lastLocation?.latitude
+                }
+            }
+
+            try {
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    null
+                )
+            } catch (e: SecurityException) {
+                // Gérer l'exception de sécurité ici
             }
         }
     }
 
     private fun stopLocationUpdates() {
-        // Arrêtez les mises à jour de la position ici si nécessaire
+        lon = null
+        lat = null
     }
 
-    private fun fetchLocation() {
-        val locationRequest = LocationRequest.create()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 5000
+    private fun checkLocationPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+    }
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            != PackageManager.PERMISSION_GRANTED && ActivityCompat
-                .checkSelfPermission(
-                    this,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                101
-            )
-            return
-        }
-
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            object : com.google.android.gms.location.LocationCallback() {
-                override fun onLocationResult(locationResult: com.google.android.gms.location.LocationResult) {
-                    val lastLocation = locationResult.lastLocation
-                    lon = lastLocation?.longitude
-                    lat = lastLocation?.latitude
-
-                    // Mettez à jour les emplacements ici
-                    updateLocations(Location(lat ?: 0.0, lon ?: 0.0))
-                }
-            },
-            null
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_CODE
         )
     }
 
-    private fun updateLocations(location: Location) {
-        lifecycleScope.launch(Dispatchers.Main) {
-            // Mettez à jour lastLocations avec les coordonnées actuelles
-            lastLocations = ArrayList(currentLocations)
-
-            // Mettez à jour currentLocations avec les nouvelles coordonnées
-            currentLocations = listOf(location)
-        }
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 101
     }
 }
