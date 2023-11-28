333fcb433080fca957dd64d48aecbd9f2f6c84e2
ninollg <nino.leliege@gmail.com>
2023-11-28 11:03:16 +0100

    vibration marche wouuhouuuu

--- app/src/main/java/com/example/hiker/MainActivity.kt
+++ app/src/main/java/com/example/hiker/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,48 +21,56 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hiker.ui.theme.HikerTheme
-import kotlinx.coroutines.flow.internal.NoOpContinuation.context
-import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 class MainActivity : ComponentActivity() {
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HikerTheme {
-                // A surface container using the 'background' color from the theme
+                // Un conteneur de surface utilisant la couleur de fond du thème
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Button(
-                            onClick = {
-                                vibrate(context)
-                            }
-                        ) {
-                            Text("Cliquez ici pour vibrer")
-                        }
+                        BoutonVibration()
                     }
                 }
             }
         }
-        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun vibrate(context: Context) {
-        val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (vibrator.hasVibrator()) {
-            val vibrationEffect = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
-            vibrator.vibrate(vibrationEffect)
+    @Composable
+    fun BoutonVibration() {
+        Button(
+            onClick = {
+                vibrer()
+            }
+        ) {
+            Text("vibrer")
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun vibrer() {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (vibrator.hasVibrator()) {
+            val vibrationEffect = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
+            vibrator.vibrate(vibrationEffect)
+        } else {
+            Log.v("vibrer", "mon telephone ne peut pas vibrer");
+        }
 
+    }
 }
-
-
