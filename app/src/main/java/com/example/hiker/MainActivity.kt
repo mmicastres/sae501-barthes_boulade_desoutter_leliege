250a7246fa4eb3880e094c5fd8bf89e8f3fe88c7
Lyrastico <louisbarthes1@gmail.com>
2023-12-01 11:54:01 +0100

    les grades par paliers de 5 niveaux

--- app/src/main/java/com/example/hiker/MainActivity.kt
+++ app/src/main/java/com/example/hiker/MainActivity.kt
@@ -7,17 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hiker.ui.theme.HikerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,15 +28,11 @@ fun Niveaux() {
     // Utilisez remember pour sauvegarder l'état même lorsque la composition est réexécutée
     val state = remember { mutableStateOf(ClassKil()) }
 
-    // Mise à jour de l'état lorsque l'utilisateur parcourt 10 kilomètres
-//    nbDeKilometreParcourus(state)
-
-
     Column(
         modifier = Modifier.fillMaxSize(),
-
-        ) {
+    ) {
         Text(text = "Niveau actuel: ${state.value.niveau}")
+        Text(text = "Grade: ${getGrade(state.value.niveau)}")
         Text(text = "Kilomètres parcourus: ${state.value.kilometres}")
         Button(
             onClick = {
@@ -54,8 +45,6 @@ fun Niveaux() {
         ) {
             Text("Parcourir 1 kilomètre")
         }
-
-
     }
 }
 
@@ -63,3 +52,16 @@ data class ClassKil(
     val niveau: Int = 1,
     val kilometres: Int = 0
 )
+
+fun getGrade(niveau: Int): String {
+    return when {
+        niveau in 1..4 -> "Apprenti randonneur"
+        niveau in 5..9 -> "Randonneur occasionnel"
+        niveau in 10..14 -> "Randonneur"
+        niveau in 15..19 -> "Randonneur expérimenté"
+        niveau in 20..24 -> "Maître randonneur"
+        niveau in 25..29 -> "Grand maître randonneur"
+        niveau in 30..34 -> "Randonneur suprême"
+        else -> "Éminent randonneur de l'ordre des Hikers"
+    }
+}
