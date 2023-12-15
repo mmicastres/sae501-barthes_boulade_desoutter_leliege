abad7be8d546e754ce8ce503317a2b1f96054d5c
Lyrastico <louisbarthes1@gmail.com>
2023-12-15 13:56:26 +0100

    rangent opti la team

--- app/src/main/java/com/example/hiker/services/LocationService.kt
+++ app/src/main/java/com/example/hiker/services/LocationService.kt
@@ -3,7 +3,6 @@ package com.example.hiker.services
 
 import android.app.Activity
 import android.content.Context
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -13,28 +12,15 @@ import com.example.hiker.utils.DistanceCalculator
 import com.google.android.gms.location.*
 import kotlinx.coroutines.launch
 
-class LocationService(private val context: Context, private val lifecycleScope: LifecycleCoroutineScope) {
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+class LocationService(
+    private val context: Context,
+    private val lifecycleScope: LifecycleCoroutineScope
+) {
+    private var fusedLocationProviderClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
     var totalDistance: Float = 0f
     var lon by mutableStateOf<Double?>(null)
     var lat by mutableStateOf<Double?>(null)
-    interface LocationUpdateObserver {
-        fun onLocationUpdated()
-    }
-
-    private val observers = mutableListOf<LocationUpdateObserver>()
-
-    fun addObserver(observer: LocationUpdateObserver) {
-        observers.add(observer)
-    }
-
-    fun removeObserver(observer: LocationUpdateObserver) {
-        observers.remove(observer)
-    }
-
-    private fun notifyObservers() {
-        observers.forEach { it.onLocationUpdated() }
-    }
 
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
@@ -49,12 +35,7 @@ class LocationService(private val context: Context, private val lifecycleScope: 
 
             lat = newLat
             lon = newLon
-            notifyObservers()
         }
-    }
-
-    init {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
     }
 
     fun toggleLocationUpdates() {
@@ -73,11 +54,12 @@ class LocationService(private val context: Context, private val lifecycleScope: 
 
     private fun startLocationUpdates() {
         lifecycleScope.launch {
-            val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).apply {
-                setMinUpdateDistanceMeters(5F)
-                setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
-                setWaitForAccurateLocation(true)
-            }.build()
+            val locationRequest =
+                LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).apply {
+                    setMinUpdateDistanceMeters(5F)
+                    setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+                    setWaitForAccurateLocation(true)
+                }.build()
 
             try {
                 fusedLocationProviderClient.requestLocationUpdates(
@@ -100,7 +82,6 @@ class LocationService(private val context: Context, private val lifecycleScope: 
             }
             lon = null
             lat = null
-            notifyObservers()
         }
     }
 
--- app/src/main/java/com/example/hiker/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/hiker/ui/activities/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.hiker.ui.activities
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -25,21 +23,6 @@ class MainActivity : ComponentActivity() {
         locationService = LocationService(this, lifecycleScope)
 
         setContent {
-            var isLocationAvailable by remember { mutableStateOf(false) }
-
-            // Souscrire aux mises à jour de localisation
-            DisposableEffect(locationService) {
-                val observer = object : LocationService.LocationUpdateObserver {
-                    override fun onLocationUpdated() {
-                        isLocationAvailable = locationService.isLocationAvailable()
-                    }
-                }
-                locationService.addObserver(observer)
-                onDispose {
-                    locationService.removeObserver(observer)
-                }
-            }
-
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -48,11 +31,11 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Button(onClick = { locationService.toggleLocationUpdates() }) {
-                    Text(text = if (isLocationAvailable) "Stop Location Updates" else "Start Location Updates")
+                    Text(text = if (locationService.isLocationAvailable()) "Stop Location Updates" else "Start Location Updates")
                 }
                 Spacer(modifier = Modifier.height(16.dp))
 
-                if (isLocationAvailable) {
+                if (locationService.isLocationAvailable()) {
                     DisplayLocation(locationService, userLevelManager)
                 }
             }
