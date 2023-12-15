1405e26b131f8045d9227db78564534a896132c6
Lyrastico <louisbarthes1@gmail.com>
2023-12-15 15:17:33 +0100

    Style changé, page statistiques

--- app/src/main/java/com/example/hiker/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/hiker/ui/activities/MainActivity.kt
@@ -3,16 +3,10 @@ package com.example.hiker.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.hiker.services.LocationService
-import com.example.hiker.ui.components.DisplayLocation
 import com.example.hiker.managers.UserLevelManager
+import com.example.hiker.ui.components.StatsPage
 
 class MainActivity : ComponentActivity() {
     private lateinit var locationService: LocationService
@@ -23,22 +17,7 @@ class MainActivity : ComponentActivity() {
         locationService = LocationService(this, lifecycleScope)
 
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick = { locationService.toggleLocationUpdates() }) {
-                    Text(text = if (locationService.isLocationAvailable()) "Stop Location Updates" else "Start Location Updates")
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-
-                if (locationService.isLocationAvailable()) {
-                    DisplayLocation(locationService, userLevelManager)
-                }
-            }
+            StatsPage(locationService, userLevelManager)
         }
     }
 }
--- app/src/main/java/com/example/hiker/ui/components/DisplayLocation.kt
+++ app/src/main/java/com/example/hiker/ui/components/DisplayLocation.kt
@@ -1,29 +1,102 @@
     package com.example.hiker.ui.components
 
-    import android.util.Log
+    import androidx.compose.foundation.background
+    import androidx.compose.foundation.layout.Arrangement
+    import androidx.compose.foundation.layout.Box
     import androidx.compose.foundation.layout.Column
+    import androidx.compose.foundation.layout.Row
     import androidx.compose.foundation.layout.Spacer
+    import androidx.compose.foundation.layout.fillMaxSize
+    import androidx.compose.foundation.layout.fillMaxWidth
     import androidx.compose.foundation.layout.height
+    import androidx.compose.foundation.layout.padding
+    import androidx.compose.foundation.layout.size
+    import androidx.compose.foundation.shape.RoundedCornerShape
+    import androidx.compose.material3.Button
     import androidx.compose.material3.Text
     import androidx.compose.ui.Modifier
     import androidx.compose.runtime.Composable
+    import androidx.compose.ui.Alignment
+    import androidx.compose.ui.graphics.Color
     import androidx.compose.ui.unit.dp
     import com.example.hiker.services.LocationService
     import com.example.hiker.managers.UserLevelManager
+    import androidx.compose.ui.text.TextStyle
+    import androidx.compose.ui.text.font.FontWeight
+    import androidx.compose.ui.unit.sp
+
+
+
 
     @Composable
-    fun DisplayLocation(locationService: LocationService, userLevelManager: UserLevelManager) {
-        val lat = locationService.lat
-        val lon = locationService.lon
-        val totalDistance = locationService.totalDistance
+    fun StatsPage(locationService: LocationService, userLevelManager: UserLevelManager) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Nom d'utilisateur: User",
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+                )
+                DisplayNiveaux(locationService.totalDistance, userLevelManager)
+                Spacer(modifier = Modifier.height(16.dp))
+                GridStatSection(locationService)
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                UniversalButton(
+                    onClickAction = { locationService.toggleLocationUpdates() },
+                    buttonText = if (locationService.isLocationAvailable()) "Stop Location Updates" else "Start Location Updates"
+                )
+            }
+        }
+    }
 
-        Column {
-            Text(text = "Current Location:")
-            Log.d("LocationService", "New lat: $lat, New lon: $lon")
-            Text(text = "Latitude: ${lat ?: 0.0}, Longitude: ${lon ?: 0.0}")
-            Text(text = "Distance parcourue: $totalDistance mètres")
+
+    @Composable
+    fun GridStatSection(locationService: LocationService) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                val formattedLat = "%.2f".format(locationService.lat ?: 0.0)
+                val formattedLon = "%.2f".format(locationService.lon ?: 0.0)
+                StatBubble(title = "Position Actuelle", content = "$formattedLat, $formattedLon")
+                StatBubble(title = "Distance Totale", content = "${locationService.totalDistance} mètres")
+            }
             Spacer(modifier = Modifier.height(16.dp))
-            DisplayNiveaux(totalDistance, userLevelManager)
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                StatBubble(title = "Duels Gagnés", content = "À venir")
+                StatBubble(title = "Collection", content = "À venir")
+            }
+        }
+    }
+
+    @Composable
+    fun StatBubble(title: String, content: String) {
+        Box(
+            modifier = Modifier
+                .size(width = 150.dp, height = 150.dp)
+                .background(color = Color.LightGray, shape = RoundedCornerShape(75.dp)),
+            contentAlignment = Alignment.Center  // Aligner le contenu au centre
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(bottom = 4.dp)  // Ajuster la marge interne
+                )
+                Text(text = content)
+            }
         }
     }
 
@@ -35,5 +108,12 @@
         Column {
             Text(text = "Niveau actuel: $level")
             Text(text = "Grade: $grade")
+        }
+    }
+
+    @Composable
+    fun UniversalButton(onClickAction: () -> Unit, buttonText: String) {
+        Button(onClick = onClickAction) {
+            Text(text = buttonText)
         }
     }
