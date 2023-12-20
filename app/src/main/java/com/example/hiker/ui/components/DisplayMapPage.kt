5004faf1584f11b4e5a1ea59f609f0532fde6949
Lyrastico <louisbarthes1@gmail.com>
2023-12-20 13:38:19 +0100

    cleen

--- app/src/main/java/com/example/hiker/ui/components/DisplayMapPage.kt
+++ app/src/main/java/com/example/hiker/ui/components/DisplayMapPage.kt
@@ -1,20 +1,17 @@
 package com.example.hiker.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.hiker.services.LocationService
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 
 @Composable
